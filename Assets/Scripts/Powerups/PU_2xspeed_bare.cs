9ab1429b700454d9192b119feb1a3caf3387a2e0
Scoan <Coan.Stephen@gmail.com>
2014-01-25 16:06:08 -0800

    salvaging operation

---
+++ Assets/Scripts/Powerups/PU_2xspeed_bare.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//valid powerup strings:
+//dblSpeed, half*, dblJump, half*, gravityInvert, controlInvert, dblFireRate, dblSize, halfSize, oneHitKills, activateHazards
+
+
+public class PU_2xspeed_bare : MonoBehaviour {
+	
+	public string powerupType;
+	public int numPowerups = 11;
+	
+	void Start() {
+	
+	int randInt = Random.Range(0,numPowerups);
+	
+	switch (randInt) {
+		
+	case 0: powerupType = "dblSpeed";
+		break;
+		case 1: powerupType = "halfSpeed";
+		break;
+		case 2: powerupType = "dblJump";
+		break;
+		case 3: powerupType = "halfJump";
+		break;
+		case 4: powerupType = "gravityInvert";
+		break;
+		case 5: powerupType = "controlInvert";
+		break;
+		case 6: powerupType = "dblFireRate";
+		break;
+		case 7: powerupType = "dblSize";
+		break;
+		
+		case 8: powerupType = "halfSize";
+		break;
+		
+		case 9: powerupType = "oneHitKills";
+		break;
+		case 10: powerupType = "activateHazards";
+		break;
+	default:
+		break;
+	}
+}
+
+void Update() {
+	
+}
+
+
+void OnCollisionEnter (Collision col) {
+	
+		if (col.gameObject.name.Contains ("Player")) {
+		
+		Player playerScript = col.gameObject.GetComponent<Player>();
+		
+		playerScript.heldPowerup = powerupType;
+		
+		JoviosControllerStyle controllerStyle = new JoviosControllerStyle();
+			controllerStyle.AddAbsoluteJoystick("left","Move Character","Move");
+			controllerStyle.AddButton2("right", new string[] {"Jump"}, new string[] {"Jump"});
+			controllerStyle.AddArbitraryButton(new int[] {-2,4,4,4}, powerupType, "powerup");
+		MenuManager.jovios.SetControls(playerScript.jUID, controllerStyle);
+		
+		Debug.Log(powerupType);
+		
+		KillPowerUp();
+		
+	}
+}
+
+void KillPowerUp() {
+	Destroy (this.gameObject);
+}
+}
